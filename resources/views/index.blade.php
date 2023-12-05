02bde960b2ce5ffaf69cb23cd0c9347c79d26e7f
shen <santi.dionis@gmail.com>
2023-12-05 20:02:34 +0100

    ADD:  based.blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.base')
+
+@section('content')
+<div class="row">
+    <div class="col-12">
+        <div>
+            <h2 class="text-white">CRUD de Tareas</h2>
+        </div>
+        <div>
+            <a href="" class="btn btn-primary">Crear tarea</a>
+        </div>
+    </div>
+
+    <div class="col-12 mt-4">
+        <table class="table table-bordered text-white">
+            <tr class="text-secondary">
+                <th>Tarea</th>
+                <th>Descripción</th>
+                <th>Fecha</th>
+                
+                <th>Estado</th>
+                <th>Acción</th>
+            </tr>
+            <tr>
+                <td class="fw-bold">Estudiar Laravel</td>
+                <td>Ver video: tu primer CRUD con laravel 10 en el canal de YouDevs</td>
+                <td>
+                    31/03/23
+                </td>
+                <td>
+                    <span class="badge bg-warning fs-6">Pendiente</span>
+                </td>
+                <td>
+                    <a href="" class="btn btn-warning">Editar</a>
+
+                    <form action="" method="post" class="d-inline">
+                        <button type="submit" class="btn btn-danger">Eliminar</button>
+                    </form>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
+@endsection
